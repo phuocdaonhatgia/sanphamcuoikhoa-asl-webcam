68e18d86c072c8a7887e71c33dd051f328f7242c
phuocdaonhatgia <119189003+phuocdaonhatgia@users.noreply.github.com>
2026-07-03 19:50:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import cv2
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from collections import Counter
+import tempfile
+import os
+import time
+import pathlib
+
+st.set_page_config(
+    page_title = "ASL Hand Sign Recognition",
+    page_icon ="🤟",
+    layout ="wide"
+)
+
+def load_css(file_path):
+    with open (file_path) as f:
+        st.html(f"<style>{f.read()}</style>)")
+
+css_path = pathlib.Path("style.css")
+load_css(css_path)
+
+def load_model():
+    model = tf.keras.models.load_model("hand_sign_model.keras")
+    class_names = sorted(['A','B','C','D','E','F','G','H','I','K',
+                          'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','del','nothing','space'])
+    return model, class_names
+
+model, class_names = load_model()
+
